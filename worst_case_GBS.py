8f7fff5c6f5cbf74e499ad1d90f95e55cbc866da
Zhenghao Li <zhenghao.li.1999@gmail.com>
2022-06-24 17:44:26 +0100

    added comments

--- worst_case_GBS.py
+++ worst_case_GBS.py
@@ -6,18 +6,23 @@ from strawberryfields.decompositions import williamson, bloch_messiah
 from src.gbs_matrices import GraphMatrices, GaussianMatrices
 from src.symplectic import Symplectic, SymplecticFock, SymplecticXXPP
 
+# Some further assumptions in the script but not in notes:
+# 1. Assume B matrix is real
+# 2. Assume B matrix diagonal terms are all zero
 
+# <<<<<<<<<<<<<<<<<<< Basic parameters  >>>>>>>>>>>>>>>>>>
 M = 6  # Number of modes
 max_degree = 4  # < M - 1
 x = - 1 / 4*(max_degree - 1) - 1  # smaller than - 1 / 4*(max_degree - 1)
 
-B_matrix = np.zeros([M,M])  # For now assume B matrix is real
-
-# We can set the gamma vector to whatever we want and find the corresponding squeezing and unitary#
+# We can set the gamma vector to whatever we want and find the corresponding squeezing and unitary
 # BUT THIS IS NOT THE DISPLACEMENT VECTOR
 half_gamma = np.ones(M)
 gamma = np.concatenate([half_gamma, half_gamma.conjugate()])
 
+
+# <<<<<<<<<<<<<<<<<<< Randomly construct B matrix  >>>>>>>>>>>>>>>>>>
+B_matrix = np.zeros([M,M])  # For now assume B matrix is real
 node_dict = {k: [] for k in range(M)}  # keys are nodes in the graph, values are connected neighbours of the corresponding node
 unfilled_nodes = list(range(M))
 for node in unfilled_nodes:
@@ -53,22 +58,28 @@ for node in unfilled_nodes:
 
     node_dict[node] = old_neighbours + new_neighbours
 
+
+# <<<<<<<<<<<<<<<<<<< Construct A matrix  >>>>>>>>>>>>>>>>>>
+# A = [[cB, 0],[0, cB]]
+
 # c factor must be smaller than inverse of the absolute value of B eig of maximum abs value
 eigs_B = np.linalg.eigvalsh(B_matrix)
 c_factor = random.uniform(0.0, 1.0) * 1 / abs(max(eigs_B, key=abs))
 
-# Construct the A matrix: A = [[cB, 0], [0, cB]]
 O_mat = np.zeros_like(B_matrix)
 A_mat = np.block([[c_factor*B_matrix, O_mat], [O_mat, c_factor*B_matrix]])
 
 assert GraphMatrices.is_valid_Amat(A_mat) == True
 
+# <<<<<<<<<<<<<<<<<<< Construct Gaussian matrices  >>>>>>>>>>>>>>>>>>
 Sigma_fock = GaussianMatrices.cov_fock_from_A(A_mat, dtype=np.float64)
 Sigma_xxpp = Symplectic.matrix_fock_to_xxpp(Sigma_fock)  # The williamson function only works for real symmetric matrix
 
 mu_fock = GaussianMatrices.mu_fock_from_A(A_mat, gamma, dtype=np.float64)
 mu_xxpp = Symplectic.vector_fock_to_xxpp(mu_fock)
 
+
+# <<<<<<<<<<<<<<<<<<< Decomposition  >>>>>>>>>>>>>>>>>>
 D, S = williamson(Sigma_xxpp)
 assert np.allclose(D, 0.5*np.identity(2*M))
 
