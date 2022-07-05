98e68ec8661222955a68022ae6a9f947ef8892c1
Zhenghao Li <zhenghao.li.1999@gmail.com>
2022-07-05 19:39:48 +0100

    add function to read pandas df file with arbitrary tail (timestamp)

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 class MatrixUtils:
 
@@ -30,3 +31,17 @@ class DFUtils:
         os.makedirs(os.path.dirname(filename), exist_ok=True)
 
         return filename
+
+    @staticmethod
+    def read_filename_head(directory, filename_head, idx=0, dt=None):
+        files = os.listdir(directory)
+
+        filtered_files = [file_ for file_ in files if file_.startswith(filename_head)]
+        file_to_read = os.path.join(directory, filtered_files[idx])
+
+        df = pd.read_csv(
+            file_to_read,
+            dtype=dt,
+        )
+
+        return df, file_to_read
