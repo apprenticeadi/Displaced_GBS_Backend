a9907908f068547a7ccc6268fe1f01fdae106847
Zhenghao Li <zhenghao.li.1999@gmail.com>
2022-07-18 21:07:30 +0100

    Added class for constructing GBS experiment

---
+++ src/gbs_experiment.py
@@ -0,0 +1,93 @@
+from strawberryfields.decompositions import takagi
+import numpy as np
+import random
+
+import interferometer as itf
+
+from src.gbs_matrices import GraphMatrices, GaussianMatrices
+from src.utils import MatrixUtils
+
+class PureGBS:
+
+    def __init__(self, M):
+        """
+        :param M: Number of modes of the experiment.
+        """
+        self.M = M
+
+    @staticmethod
+    def random_interferometer(M, depth):
+        I = itf.Interferometer()
+
+        for k in range(depth):
+            p = M // 2
+            q = M % 2
+            if k % 2 != 0 and q == 0:
+                shift = 1
+            else:
+                shift = 0
+
+            for i in range(p - shift):
+                j = 2 * i + 1 + k % 2  # Clements interferometer mode index starts from 1
+                phase = 0.5 * random.random() * np.pi
+                angle = 0.5 * random.random() * np.pi
+
+                bs = itf.Beamsplitter(j, j + 1, angle, phase)
+
+                I.add_BS(bs)
+
+        return I
+
+    def add_interferometer(self, U):
+
+        if not GaussianMatrices.is_valid_U(U):
+            raise ValueError('Input matrix is not valid unitary interferometer')
+
+        n = U.shape[0]
+        if n != self.M:
+            raise ValueError('Input matrix should have dimensions (M*M)')
+
+        self.U = U
+
+    def add_squeezing(self, rs):
+        rs = np.asarray(rs)
+        self.rs = rs
+
+    def add_coherent(self, alphas):
+        alphas = np.asarray(alphas)
+        self.alphas = alphas
+
+    def calc_B(self):
+        D = np.identity(self.M)
+        v = np.tanh(self.rs)
+        D = MatrixUtils.filldiag(D, v)
+
+        return self.U @ D @ self.U.T
+
+    def calc_A(self):
+        B = self.calc_B()
+        return GraphMatrices.pure_A_from_B(B)
+
+
+    def calc_cov_fock(self):
+        A = self.calc_A()
+
+        return GaussianMatrices.cov_fock_from_A(A)
+
+    def calc_d_fock(self):
+        """
+        :return: displacement vector in Fock basis
+        """
+
+        return np.concatenate([self.alphas, self.alphas.conjugate()])
+
+    def calc_Gamma(self):
+
+        cov_fock = self.calc_cov_fock()
+
+        return GraphMatrices.Gamma(cov_fock, self.calc_d_fock())
+
+
+
+
+
--- test/test_interferometer.py
+++ test/test_interferometer.py
@@ -2,33 +2,15 @@ import interferometer as itf
 import numpy as np
 import random
 
-M = 10
-I = itf.Interferometer()
-angles = []
-phases = []
+from src.gbs_experiment import PureGBS
 
-for depth in range(10):
-
-    if depth % 2 == 0:
-        shift = 0
-    else:
-        shift = 1
-
-    for i in range(M // 2 - shift):
-        j = 2*i + shift + 1  # Interferometer mode index starts from 1
-        phase = 0.5 * random.random() * np.pi
-        angle = 0.5 * random.random() * np.pi
-
-        angles.append(angle)
-        phases.append(phase)
-
-        bs = itf.Beamsplitter(j, j+1, angle, phase)
-
-        I.add_BS(bs)
+M = 20
+depth =5
+I = PureGBS.random_interferometer(M, depth = depth)
 
 I.draw()
 
 U = I.calculate_transformation()
 print(U)
 
-np.savetxt('testU', U, delimiter=',')
+# np.savetxt('testU', U, delimiter=',')
