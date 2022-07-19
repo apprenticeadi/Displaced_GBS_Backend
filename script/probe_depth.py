687b56cf8ea33fa824a95b9d2fdee64ce01898d1
Zhenghao Li <zhenghao.li.1999@gmail.com>
2022-07-19 18:57:39 +0100

    Solve problem at high squeezing

--- script/probe_depth.py
+++ script/probe_depth.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 from src.gbs_experiment import PureGBS
 from src.utils import MatrixUtils
 
-M = 30
-depth = 10
+M = 20
+depth = 5
 
-r = random.random()
+r = 5
 rs = np.ones(M) * r
 
 alpha = 1
@@ -47,9 +47,9 @@ dmax = max(degree_sequence)
 
 fig = plt.figure("Degree of a random graph", figsize=(8, 8))
 # Create a gridspec for adding subplots of different sizes
-axgrid = fig.add_gridspec(5, 4)
+axgrid = fig.add_gridspec(6, 6)
 
-ax0 = fig.add_subplot(axgrid[0:3, :])
+ax0 = fig.add_subplot(axgrid[0:3, :3])
 Gcc = G.subgraph(sorted(nx.connected_components(G), key=len, reverse=True)[0])
 pos = nx.spring_layout(Gcc, seed=10396953)
 nx.draw_networkx_nodes(Gcc, pos, ax=ax0, node_size=20)
@@ -57,13 +57,32 @@ nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.4)
 ax0.set_title("Graph for M={}, depth={}".format(M, depth))
 ax0.set_axis_off()
 
-ax1 = fig.add_subplot(axgrid[3:, :2])
+
+
+x = x.flatten()
+x_max = abs(max(x, key=abs))
+axislim = max(1, x_max+1)
+
+ax3 = fig.add_subplot(axgrid[0:3, 3:])
+ax3.plot(x.real, x.imag, marker='.', linestyle='None')
+ax3.grid(True, which='both')
+ax3.axhline(y=0, color='k')
+ax3.axvline(x=0, color='k')
+circ = plt.Circle((0, 0), radius=0.125, edgecolor='b', facecolor='None')
+ax3.add_patch(circ)
+ax3.set_title('Edge activities')
+ax3.set_xlabel('Re(x)')
+ax3.set_ylabel('Imag(x)')
+ax3.set_xlim(-axislim, axislim)
+ax3.set_ylim(-axislim, axislim)
+
+ax1 = fig.add_subplot(axgrid[3:, :3])
 ax1.plot(degree_sequence, "b-", marker="o")
 ax1.set_title("Degree Rank Plot")
 ax1.set_ylabel("Degree")
 ax1.set_xlabel("Rank")
 
-ax2 = fig.add_subplot(axgrid[3:, 2:])
+ax2 = fig.add_subplot(axgrid[3:, 3:])
 ax2.bar(*np.unique(degree_sequence, return_counts=True))
 ax2.set_title("Degree histogram")
 ax2.set_xlabel("Degree")
