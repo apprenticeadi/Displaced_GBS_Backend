e0277185ab29ad5f5691250e52801cdbf2cb7c78
Zhenghao Li <zhenghao.li.1999@gmail.com>
2022-07-25 18:18:43 +0100

    Rewrite to probe v

--- script/probe_v_vector.py
+++ script/probe_v_vector.py
@@ -6,108 +6,90 @@ import matplotlib.pyplot as plt
 
 from strawberryfields.decompositions import takagi
 
-from src.utils import MatrixUtils, DFUtils
-from src.random_graph import RandomGraph
-from src.gbs_matrices import GraphMatrices, GaussianMatrices
-from src.log_utils import LogUtils
+from src.utils import MatrixUtils, DFUtils, LogUtils, RandomUtils
+from src.gbs_matrix import GBSMatrix, GaussianMatrix
+from src.adjacency_graph import MatchingGraph
 
 # <<<<<<<<<<<<<<<<<<< Parameters  >>>>>>>>>>>>>>>>>>
-x = 1  # edge activity
-r_max = 1  # maximum squeezing available in experiment
-gamma_low = 1
-gamma_high = np.sqrt(10)
-M_list = [5, 8, 10, 13, 15, 18, 20]
-log_diag_weights = list(range(-5, 6))  # log(w)
+vweights = [0.01, 0.05, 0.1, 0.3, 0.5, 0.8, 1, 2, 3, 4, 5, 10, 50, 100, 500, 1000]  # edge activity
+Ms = [10, 20, 30, 50, 100]
 
-if x < 0:
-    complexity = 'hard'
-else:
-    complexity = 'easy'
+# <<<<<<<<<<<<<<<<<<< Fixed parameters (if any)  >>>>>>>>>>>>>>>>>>
+x=-1
+delta = 5
+r_max = 0.75
+
 # <<<<<<<<<<<<<<<<<<< Logging  >>>>>>>>>>>>>>>>>>
 time_stamp = datetime.datetime.now().strftime("%d-%m-%Y(%H-%M-%S.%f)")
-dir = r'..\Results\probe_even_v\x={}_{}'.format(x, time_stamp)
+dir = r'..\Results\probe_v\{}'.format(time_stamp)
 LogUtils.log_config(time_stamp='', dir=dir, filehead='log', module_name='', level=logging.INFO)
 
-logging.info('edge activity x={}, r_max={}, gamma_low={}, gamma_high={}, M_list={}, log_diag_weights={}'
-             .format(x, r_max, gamma_low, gamma_high, M_list, log_diag_weights))
-
+logging.info('Probing the physical meaning of v vector. Mode M ={}. Fix max_degree={}, r_max={}, edge activity x={}. '
+             'v=[1,...1]*vweight, where vweight from {}'.format(
+    Ms, delta, r_max, x, vweights
+))
 
 # <<<<<<<<<<<<<<<<<<< Plotting  >>>>>>>>>>>>>>>>>>
 # sq photons
 fig1, ax1 = plt.subplots()
 ax1.set_title('Average squeezed photons')
-ax1.set_xlabel('log(w)')
+ax1.set_xlabel('v')
 ax1.set_ylabel('n_sq')
+ax1.set_xscale('log')
 color_cycler = fig1.gca()._get_lines.prop_cycler
 
 # cl photons
 fig2, ax2 = plt.subplots()
 ax2.set_title('Average classical photons')
-ax2.set_xlabel('log(w)')
+ax2.set_xlabel('v')
 ax2.set_ylabel('n_cl')
 ax2.set_yscale('log')
+ax2.set_xscale('log')
 
 # sq/cl
 fig3, ax3 = plt.subplots()
 ax3.set_title('Photon number ratio squeezed/classical')
-ax3.set_xlabel('log(w)')
+ax3.set_xlabel('v')
 ax3.set_ylabel('n_sq/n_cl')
 ax3.set_yscale('log')
+ax3.set_xscale('log')
 
-for M in M_list:
-    # <<<<<<<<<<<<<<<<<<< Graph  >>>>>>>>>>>>>>>>>>
-    delta = max(3, int(0.5 * M))  # maximum degree
-    graph_G = RandomGraph(M=M, max_degree=delta)
+for M in Ms:
 
-    logging.info('')
-    logging.info('M={} and max degree={}'.format(M, delta))
-    logging.info('Graph adjacency matrix is \n{}'.format(graph_G.adjacency_matrix()))
+    # <<<<<<<<<<<<<<<<<<< Fixed parameters  >>>>>>>>>>>>>>>>>>
+    half_gamma = np.ones(M)
 
-    # <<<<<<<<<<<<<<<<<<< half gamma vector  >>>>>>>>>>>>>>>>>>
-    half_gamma = np.random.uniform(low=gamma_low, high=gamma_high, size=M)
-    Bmat = graph_G.generate_Bmatrix(x, half_gamma)
+    adj = RandomUtils.random_adj(M, delta)
+    graph = MatchingGraph(adj, half_gamma=half_gamma, r_max=r_max, x=x)
 
-    logging.info('half_gamma={}'.format(half_gamma))
+    logging.info('M={}, half_gamma={}'.format(
+        M, half_gamma,
+    ))
 
-    # <<<<<<<<<<<<<<<<<<< Data file  >>>>>>>>>>>>>>>>>>
+    np.save(dir + r'\adj_M={}_delta={}'.format(M, delta), adj)
+
     save_filename = DFUtils.create_filename(
-        dir + r'\M={}_delta={}'.format(M, delta))
+        dir + r'\M={}_delta={}.csv'.format(M, delta))
     sq_phots = []
     coh_phots = []
 
-    for idx, log_diag_weight in enumerate(log_diag_weights):
+    for idx, vweight in enumerate(vweights):
+        v = np.ones(M) * vweight
 
-        # <<<<<<<<<<<<<<<<<<< B diagonal >>>>>>>>>>>>>>>>>>
-        v = np.ones(M) * 10 ** log_diag_weight  # np.random.uniform(low=0, high=1, size=M) * 100000
-        Bmat = MatrixUtils.filldiag(Bmat, v)
-        logging.info('For diag_weight={}, v = {}'.format(10 ** log_diag_weight, v))
+        graph.set_v(v)
+        sq, displacement, U = graph.generate_experiment()
 
-        # <<<<<<<<<<<<<<<<<<< rescale cB and Gamma >>>>>>>>>>>>>>>>>>
-        eigs_B = np.linalg.eigvalsh(Bmat)
-        c_factor = np.tanh(r_max) / abs(max(eigs_B, key=abs))
-        cB = c_factor * Bmat
-        Gamma = np.sqrt(c_factor) * np.concatenate([half_gamma, half_gamma.conjugate()])
+        logging.info('vweight={}, sq = {}, displacement = {}'.format(vweight, sq, displacement))
+        np.save(dir+r'\U_M={}_delta={}'.format(M, delta), U)
 
-        # <<<<<<<<<<<<<<<<<<< experimental parameters >>>>>>>>>>>>>>>>>>
-        Amat = GraphMatrices.pure_A_from_B(cB)
-        mu_fock = GaussianMatrices.mu_fock_from_A(Amat, Gamma)
-        tanhr, U = takagi(cB)
-
-        sq = np.arctanh(tanhr)
-        displacement = mu_fock[:M]
-        logging.info('sq = {}, displacement = {}'.format(sq, displacement))
-
-        sq_phot = np.sum(np.sinh(sq) ** 2)
-        coh_phot = np.sum(displacement * displacement.conjugate()).real
-
-        sq_phots.append(sq_phot)
-        coh_phots.append(coh_phot)
+        sq_phots.append(np.sum(np.sinh(sq) ** 2))
+        coh_phots.append(np.sum(displacement * displacement.conjugate()).real)
 
     sq_phots = np.array(sq_phots)
     coh_phots = np.array(coh_phots)
 
     df = pd.DataFrame({
-        'log_diag_weight': log_diag_weights,
+        'v': vweights,
         'sq_photons': sq_phots,
         'coh_photons': coh_phots
     })
@@ -115,15 +97,15 @@ for M in M_list:
 
     # Plot sq photons
     color = next(color_cycler)['color']
-    ax1.plot(log_diag_weights, sq_phots, color=color, label='M={}'.format(M))
-    ax1.plot(log_diag_weights, [M * np.sinh(r_max) ** 2] * len(log_diag_weights),
+    ax1.plot(vweights, sq_phots, color=color, label='M={}'.format(M))
+    ax1.plot(vweights, [M * np.sinh(r_max) ** 2] * len(vweights),
              color=color, linestyle='--')
 
     # Plot coh photons
-    ax2.plot(log_diag_weights, coh_phots, label='M={}'.format(M))
+    ax2.plot(vweights, coh_phots, label='M={}'.format(M))
 
     # Plot sq/coh
-    ax3.plot(log_diag_weights, sq_phots / coh_phots, label='M={}'.format(M))
+    ax3.plot(vweights, sq_phots / coh_phots, label='M={}'.format(M))
 
 ax1.legend()
 fig1.savefig(dir+r'\sq_phot.pdf')
@@ -132,4 +114,4 @@ fig2.savefig(dir+r'\cl_phot.pdf')
 ax3.legend()
 fig3.savefig(dir+r'\sq_div_cl.pdf')
 
-logging.info('Porsche')
+logging.info('Aston Martin')
