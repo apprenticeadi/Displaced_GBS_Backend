2de4b1b881f89f5c78b44e25bb4034e04b58fae3
Zhenghao Li <zhenghao.li.1999@gmail.com>
2022-07-25 19:33:07 +0100

    new .gitignore

--- test/test_random_graph.py
+++
@@ -1,9 +0,0 @@
-from src.random_graph import RandomGraph
-
-test_graph = RandomGraph(5, 3)
-
-adj = test_graph.adjacency_matrix()
-
-print(adj)
-print(test_graph.degrees())
-print(test_graph.degree_counts())
