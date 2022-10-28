fd6738980448fe8518b716cea348268e81969745
Zhenghao Li <zhenghao.li.1999@gmail.com>
2022-10-28 19:39:55 +0100

    add sud and sdu model

--- src/gbs_experiment.py
+++ src/gbs_experiment.py
@@ -7,7 +7,7 @@ import interferometer as itf
 
 from src.gbs_matrix import GBSMatrix, GaussianMatrix
 from src.utils import MatrixUtils
-from src.symplectic import SymplecticFock
+from src.symplectic import SymplecticXXPP
 
 
 # Construct experiment
@@ -17,48 +17,57 @@ from src.symplectic import SymplecticFock
 
 class PureGBS:
 
-    def __init__(self, M, dtype=np.complex64):
+    # Everything in xxpp basis unless otherwise specified, to reduce numerical error.
+
+    def __init__(self, M):
         """
         :param M: Number of modes of the experiment.
         """
         self.M = M
 
-        self._cov_fock = 0.5 * np.identity(2 * M, dtype=dtype)  # Vacuum state
-        self._means_fock = np.zeros(2 * M, dtype=dtype)  # Means vector
+        self._cov = 0.5 * np.identity(2 * M)  # Vacuum state
+        self._means = np.zeros(2 * M)  # Means vector
 
     def add_interferometer(self, U):
 
-        S = SymplecticFock.interferometer(U)
-        self._means_fock = S @ self._means_fock
-        self._cov_fock = S @ self._cov_fock @ S.T.conjugate()
+        S = SymplecticXXPP.interferometer(U)
+        self._means = S @ self._means
+        self._cov = S @ self._cov @ S.T
 
     def add_squeezing(self, rs):
+        """Add single mode squeezing """
         s = np.absolute(rs)
         theta = np.angle(rs)
 
-        S = SymplecticFock.single_mode_squeezing(s, theta)
-        self._means_fock = S @ self._means_fock
-        self._cov_fock = S @ self._cov_fock @ S.T.conjugate()
+        S = SymplecticXXPP.single_mode_squeezing(s, theta)
+        self._means = S @ self._means
+        self._cov = S @ self._cov @ S.T
 
-    def add_two_mode_squeezing(self, r, mode_pair):
-        s = np.absolute(r)
-        theta = np.angle(r)
+    def add_two_mode_squeezing(self, rs):
+        """
+        Can only accept real squeezing parameters.
+        """
+        rs = np.atleast_1d(rs)
+        if any(rs.imag != 0):
+            raise Warning('Only accept real squeezing parameters. Automatically discard imaginary parts')
+        rs = rs.real
 
-        S = SymplecticFock.two_mode_squeezing(s, self.M, mode_pair, theta)
-        self._means_fock = S @ self._means_fock
-        self._cov_fock = S @ self._cov_fock @ S.T.conjugate()
+        S = SymplecticXXPP.two_mode_squeezing(rs)
+        self._means = S @ self._means
+        self._cov = S @ self._cov @ S.T
 
     def add_displacement(self, alphas):
-        d = np.concatenate([alphas, alphas.conjugate()])
-        self._means_fock += d
+        alphas = np.atleast_1d(alphas)
+        d = np.sqrt(2) * np.concatenate([alphas.real, alphas.imag])
+        self._means += d
 
-    def state_fock(self):
-        return copy.deepcopy(self._means_fock), copy.deepcopy(self._cov_fock)
+    def state_xxpp(self):
+        return copy.deepcopy(self._means), copy.deepcopy(self._cov)
 
     def calc_A(self):
-        _, cov_fock = self.state_fock()
+        _, cov_xxpp = self.state_xxpp()
 
-        A = GBSMatrix.Amat(cov_fock)
+        A = GBSMatrix.Amat(cov_xxpp)
 
         return A
 
@@ -68,34 +77,65 @@ class PureGBS:
 
         return A[:M, :M]
 
+    def calc_Gamma(self):
+        means_xxpp, cov_xxpp = self.state_xxpp()
+        return GBSMatrix.Gamma(cov_xxpp, means_xxpp)
 
-class TakagiGBS(PureGBS):
+    def calc_half_gamma(self):
+        Gamma = self.calc_Gamma()
+        return Gamma[:self.M]
 
-    def __init__(self, M, dtype=np.complex64):
+#Deprecation
+def TakagiGBS(*args, **kwargs):
+    from warnings import warn
+    warn("TakagiGBS deprecated. Get with the program!")
+    return sudGBS(*args, **kwargs)
+
+
+class sudGBS(PureGBS):
+
+    def __init__(self, M):
         """
         :param M: Number of modes of the experiment.
         """
-        super().__init__(M, dtype)
+        super().__init__(M)
         self.M = M
         self._B = None
-        self.alphas = np.zeros(M, dtype=dtype)
-        self.rs = np.zeros(M, dtype=dtype)
-        self.U = np.identity(M, dtype=dtype)
+        self.alphas = np.zeros(M)
+        self.rs = np.zeros(M)
+        self.U = np.identity(M)
+
+    def _calc_cov(self):
+        A = self.calc_A()
+        self._cov = GaussianMatrix.cov_xxpp(A)
+
+    def _calc_means(self):
+        self._means = np.sqrt(2) * np.concatenate([self.alphas.real, self.alphas.imag])
+
+    def state_xxpp(self):
+        self._calc_means()
+        self._calc_cov()
+        return copy.deepcopy(self._means), copy.deepcopy(self._cov)
 
     def add_interferometer(self, U):
 
         self.U = U
-        super().add_interferometer(U)
+        # super().add_interferometer(U)  # We want to avoid calculating the cov and means, otherwise no point in the takagi shortcut
 
     def add_squeezing(self, rs):
-        rs = np.asarray(rs)
+        """Add single mode squeezing"""
+        rs = np.atleast_1d(rs)
+        if any(rs.imag != 0):
+            raise Warning('Only accept real squeezing parameters. Automatically discard imaginary parts')
+        rs = rs.real
         self.rs = rs
-        super().add_squeezing(rs)
+        # super().add_squeezing(rs)
 
     def add_displacement(self, alphas):
+        """Add displacement after interferometer"""
         alphas = np.asarray(alphas)
         self.alphas = alphas
-        super().add_displacement(alphas)
+        # super().add_displacement(alphas)
 
     def calc_B(self):
 
@@ -124,7 +164,7 @@ class TakagiGBS(PureGBS):
 
     def calc_half_gamma(self):
         B = self.calc_B()
-        half_gamma = self.alphas.conjugate() - self.alphas @ B
+        half_gamma = self.alphas.conjugate() - B.conjugate() @ self.alphas
 
         return half_gamma
 
@@ -149,3 +189,17 @@ class TakagiGBS(PureGBS):
 
         x = B / np.outer(half_gamma, half_gamma)
         return x
+
+
+class sduGBS(sudGBS):
+
+    # Override
+    def add_displacement(self, betas):
+        """Add displacement before interferometer"""
+        betas = np.asarray(betas)
+        self.alphas = betas
+
+    # Override
+    def add_interferometer(self, U):
+        self.U = U
+        self.alphas = U.conjugate() @ self.alphas
