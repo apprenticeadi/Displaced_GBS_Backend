46d5b7c9ebdad9883e75d6b776780228fb346ba4
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-03-23 11:51:26 +0000

    parallelize determinant calculation

--- script/anticoncentration.py
+++ script/anticoncentration.py
@@ -14,7 +14,7 @@ from thewalrus import perm, hafnian
 
 def Gaussian_lhafs(w, N, var, repeats, func='lhaf', print_bool=False):
     """
-    Calculates |lhaf(XX^T, w sumX)| or |haf(XX^T)| or |Per(X)| or |Det(X)| for N*N complex Gaussian matrix X from G(0,var)
+    Calculates |lhaf(XX^T, w sumX)| or |haf(XX^T)| or |Per(X)| for N*N complex Gaussian matrix X from G(0,var)
     :param w: diagonal weight
     :param N: Dimension of matrix
     :param var: Variance of Gaussian matrix
@@ -59,6 +59,32 @@ def Gaussian_lhafs(w, N, var, repeats, func='lhaf', print_bool=False):
     return raw
 
 
+@numba.njit(parallel=True)
+def Gaussian_dets(N, var, repeats, func='det'):
+    """
+    Calculates |Det(X)| for N*N complex Gaussian matrix X from G(0,var). This can be parallelized.
+    :param N: Dimension of matrix
+    :param var: Variance of Gaussian matrix
+    :param repeats: Number of repetitions
+    :param func: 'det'
+    :param print_bool: whether print time to console
+
+    :return: array of length `repeats'
+    """
+
+    raw = np.zeros(repeats)
+    for i in numba.prange(repeats):
+        X = np.random.normal(loc=0, scale=np.sqrt(var) / np.sqrt(2), size=(N, N)) + \
+            1j * np.random.normal(loc=0, scale=np.sqrt(var) / np.sqrt(2), size=(N, N))
+
+        if func == 'det':
+            raw_i = np.absolute(np.linalg.det(X))
+        else:
+            raise ValueError('Func not recognized')
+        raw[i] = raw_i
+
+    return raw
+
 # <<<<<<<<<<<<<<<<<<< Basic parameters  >>>>>>>>>>>>>>>>>>
 # func = 'lhaf'
 # if func == 'lhaf':
@@ -66,8 +92,8 @@ def Gaussian_lhafs(w, N, var, repeats, func='lhaf', print_bool=False):
 # else:
 #     w = 0
 var = 1  # For now we only care about Gaussian matrices with variance 1. This will involve some rescaling of the matrices.
-Ns = np.arange(6, 30, step=2)
-total_repeats = 100000  # Take some integer multiple of 1000
+Ns = np.arange(30, 50, step=1)
+total_repeats = 100000 # Take some integer multiple of 1000
 print_bool = False
 
 # <<<<<<<<<<<<<<<<<<< Logging  >>>>>>>>>>>>>>>>>>
@@ -83,9 +109,8 @@ logging.info(
 
 
 # <<<<<<<<<<<<<<<<<<< Calculating  >>>>>>>>>>>>>>>>>>
-# Unable to parallelize this
-# @numba.jit(parallel=True)
-def wrapper_parallel(N, sub_repeats=1000, func='lhaf', w=0, w_string='0'):
+# Unable to parallelize this for loop Hafnian and permanent
+def wrapper(N, sub_repeats=1000, func='lhaf', w=0, w_string='0'):
     if total_repeats % sub_repeats != 0:
         raise ValueError('Please make my life easier by making total repeats an integer multiple of sub_repeats')
 
@@ -102,22 +127,25 @@ def wrapper_parallel(N, sub_repeats=1000, func='lhaf', w=0, w_string='0'):
         save_dir = dir_head + fr'\{func}_{time_stamp}\N={N}'
 
     n = total_repeats // sub_repeats
-    # for i in numba.prange(n):
+
     for i in range(n):
         t_i = time.time()
-        raw = Gaussian_lhafs(w, N, var, sub_repeats, func, print_bool)
+        if func == 'det':
+            raw = Gaussian_dets(N, var, sub_repeats, func)
+        else:
+            raw = Gaussian_lhafs(w, N, var, sub_repeats, func, print_bool)
         np.save(DFUtils.create_filename(save_dir + rf'\raw_{i}.npy'), raw)
         t_f = time.time()
-        logging.info(f'Calculate {i}-th batch {sub_repeats} {func}s for N={N}, w={w} took time={t_f - t_i}')
+        logging.info(f'Calculate {i}-th batch {sub_repeats} {func}s for N={N}, w={float(w):.3} took time={t_f - t_i}')
 
 
 for N in Ns:
-    wrapper_parallel(N, sub_repeats=1000, func='det')
-for N in Ns:
-    wrapper_parallel(N, sub_repeats=1000, func='lhaf', w=1)
-for N in Ns:
-    wrapper_parallel(N, sub_repeats=1000, func='haf')
-for N in Ns:
-    wrapper_parallel(N, sub_repeats=1000, func='perm')
-for N in Ns:
-    wrapper_parallel(N, sub_repeats=1000, func='lhaf', w=1/N, w_string='1/N')
+    wrapper(N, sub_repeats=1000, func='det')
+# for N in Ns:
+#     wrapper_parallel(N, sub_repeats=1000, func='lhaf', w=1)
+# for N in Ns:
+#     wrapper_parallel(N, sub_repeats=1000, func='haf')
+# for N in Ns:
+#     wrapper_parallel(N, sub_repeats=1000, func='perm')
+# for N in Ns:
+#     wrapper_parallel(N, sub_repeats=1000, func='lhaf', w=1/N, w_string='N^-1')
