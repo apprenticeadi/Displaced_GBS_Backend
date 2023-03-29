2a5f313e3ed35dc68243e4a14552466ebd35d19e
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-03-29 16:17:26 +0100

    updated plotting script for acc over x

--- plotting/plot_acc_over_x.py
+++ plotting/plot_acc_over_x.py
@@ -12,110 +12,123 @@ def prefactor(N, func):
         return 1 / np.sqrt(factorial(N))
     elif func == 'haf':
         if N % 2 ==0 :
-            return np.sqrt(comb(N**2 + N - 1, N) / comb(N-1, N/2) / np.power(float(N), 2.*N))
+            return 1 / np.sqrt(comb(N-1, N/2) * factorial(N))
         else:
             return 0
 
     elif func[:4] == 'lhaf':
-        w = float(func[func.index('w')+2:])
-        return np.sqrt(comb(N**2 + N-1, N) * np.power(2./N**2, float(N)) / big_F(w, N, N)[N])
+        if func[func.index('w')+2:] == "N^-1":
+            w = 1 / N
+        else:
+            w = float(func[func.index('w')+2:])
+        return np.sqrt( np.power(2., float(N)) / factorial(N) / big_F(w, N, N)[N])
 
     else:
         raise ValueError('func not recognized')
 
-
-
-funcs = ['det', 'perm', 'haf', 'lhaf_w=1']
-repeat_i = 1000  #number of data in each raw data file
-repeats=100000
-N = 20
-
-dir_head = fr'..\Results\anticoncentration_over_X\{repeats}repeats'
-func_dirs = os.listdir(dir_head)
-
-plt.figure('Refactorised data')
-raw_data_dict = {}
-data_dict = {}
-for func in funcs:
-    filtered_dir = [file_ for file_ in func_dirs if file_.startswith(func)][0]  # For now just take the first directory, but later need to modify this when data become more complicated
-    N_dir = dir_head + rf'\{filtered_dir}\N={N}'
-
-    raw_data_files = os.listdir(N_dir)
-
-    combined_raw_data = np.zeros((len(raw_data_files), repeat_i), dtype=float)
-
-    for i, fn in enumerate(raw_data_files):
-        combined_raw_data[i, :] = np.load(N_dir + fr'\{fn}')
-
-    combined_raw_data = combined_raw_data.flatten()
-    combined_raw_data.sort()
-    raw_data_dict[func] = combined_raw_data
-
-    pf = prefactor(N, func)
-    refactored_data = pf * combined_raw_data
-    data_dict[func] = refactored_data
-
-    plt.plot(list(range(repeats)), refactored_data, label=func)
-
-plt.xticks([1, repeats])
-plt.legend()
-plt.yscale('log')
-
-
-# Here I refactorise the data such that their second moment is 1. Recipe known for permanent and det but not for lhaf and haf
-renorm_data_dict = {}
-plt.figure('Renormalised data')
-for func in raw_data_dict.keys():
-    second_moment = np.mean(raw_data_dict[func]**2)
-    renorm_data = raw_data_dict[func] / np.sqrt(second_moment)
-    renorm_data_dict[func] = renorm_data
-    plt.plot(list(range(repeats)), renorm_data, label=func)
-
-plt.xticks([1, repeats])
-plt.legend()
-plt.yscale('log')
-
-# squared data and probability density function
-x_min = np.min(data_dict['lhaf_w=1'])**2
-x_min_log = np.floor(np.log10(x_min))  # the minimum value much more likely to come from lhaf with w=1
-xs = np.logspace(start=x_min_log, stop=1, num=16*int(np.absolute(x_min_log)))  # evenly distributed in log scale, 16 per decade
-
-prob_density_function = {}  # for the squared data
-prob_density_function['xs'] = xs[1:-1]  # f(x_i) = (F(x_i+1) - F(x_i-1) ) / (x_i+1 - x_i-1)
-prob_distrib_function = {}
-prob_distrib_function['xs'] = xs
-squared_data = {}
-
-for func in data_dict.keys():  # use refactorised not renormalised data
-    func_squared_data = data_dict[func] ** 2
-    squared_data[func] = func_squared_data  # This is already sorted
-
-    func_distrib = np.zeros_like(xs, dtype=float)
-    for i, x in enumerate(xs):
-        func_distrib[i] = np.argmax(func_squared_data > x) / len(func_squared_data)
-    prob_distrib_function[func] = func_distrib
-
-    func_pdf = np.zeros(len(xs) - 2, dtype=float)
-    for i,x in enumerate(xs[1:-1]):
-        func_pdf[i] = (func_distrib[i+1] - func_distrib[i-1]) / (xs[i+1] - xs[i-1])
-    prob_density_function[func] = func_pdf
-
-    plt.figure('distribution')
-    plt.plot(xs, func_distrib, '.', label=func)
-
-    plt.figure('pdf')
-    arg_nonzero = np.where(func_pdf > 0)
-    plt.plot(xs[1:-1][arg_nonzero], func_pdf[arg_nonzero], label=func)
-
-plt.figure('distribution')
-plt.title('Probability distribution')
-plt.legend()
-plt.yscale('log')
-plt.xscale('log')
-
-plt.figure('pdf')
-plt.title('Probability density function')
-plt.legend()
-plt.yscale('log')
-plt.xscale('linear')
-plt.xlim(-0.1, 2)
+#
+#
+# funcs = ['det', 'perm', 'haf', 'lhaf_w=1', 'lhaf_w=N^-1']
+# func_labels={
+#     'det': r'Det',
+#     'perm': r'Per',
+#     'haf': r'Haf',
+#     'lhaf_w=1': r'lHaf$(w=1)$',
+#     'lhaf_w=N^-1': r'lHaf$(w=\frac{1}{N})$'
+# }
+#
+# repeat_i = 1000  #number of data in each raw data file
+# repeats=100000
+# N = 20
+#
+# dir_head = fr'..\Results\anticoncentration_over_X\{repeats}repeats'
+# func_dirs = os.listdir(dir_head)
+#
+# plt.figure(f'Refactorised data N={N}')
+# raw_data_dict = {}
+# data_dict = {}
+# for func in funcs:
+#     filtered_dir = [file_ for file_ in func_dirs if file_.startswith(func)][0]  # For now just take the first directory, but later need to modify this when data become more complicated
+#     N_dir = dir_head + rf'\{filtered_dir}\N={N}'
+#
+#     raw_data_files = os.listdir(N_dir)
+#
+#     combined_raw_data = np.zeros((len(raw_data_files), repeat_i), dtype=float)
+#
+#     for i, fn in enumerate(raw_data_files):
+#         combined_raw_data[i, :] = np.load(N_dir + fr'\{fn}')
+#
+#     combined_raw_data = combined_raw_data.flatten()
+#     combined_raw_data.sort()
+#     raw_data_dict[func] = combined_raw_data
+#
+#     pf = prefactor(N, func)
+#     refactored_data = pf * combined_raw_data
+#     data_dict[func] = refactored_data
+#
+#     plt.plot(list(range(repeats)), refactored_data, label=func_labels[func])
+#
+# plt.xticks([1, repeats])
+# plt.legend()
+# plt.yscale('log')
+#
+#
+# # # Here I refactorise the data such that their second moment is 1. Recipe known for permanent and det but not for lhaf and haf
+# # renorm_data_dict = {}
+# # plt.figure(f'Renormalised data N={N}')
+# # for func in raw_data_dict.keys():
+# #     second_moment = np.mean(raw_data_dict[func]**2)
+# #     renorm_data = raw_data_dict[func] / np.sqrt(second_moment)
+# #     renorm_data_dict[func] = renorm_data
+# #     plt.plot(list(range(repeats)), renorm_data, label=func)
+# #
+# # plt.xticks([1, repeats])
+# # plt.legend()
+# # plt.yscale('log')
+#
+# # squared data and probability density function
+# x_min = np.min(data_dict['lhaf_w=1'])**2
+# x_min_log = np.floor(np.log10(x_min))  # the minimum value much more likely to come from lhaf with w=1
+# xs = np.logspace(start=x_min_log, stop=1, num=16*int(np.absolute(x_min_log)))  # evenly distributed in log scale, 16 per decade
+#
+# prob_density_function = {}  # for the squared data
+# prob_density_function['xs'] = xs[1:-1]  # f(x_i) = (F(x_i+1) - F(x_i-1) ) / (x_i+1 - x_i-1)
+# prob_distrib_function = {}
+# prob_distrib_function['xs'] = xs
+# squared_data = {}
+#
+# for func in data_dict.keys():  # use refactorised not renormalised data
+#     func_squared_data = data_dict[func] ** 2
+#     squared_data[func] = func_squared_data  # This is already sorted
+#
+#     func_distrib = np.zeros_like(xs, dtype=float)
+#     for i, x in enumerate(xs):
+#         func_distrib[i] = np.argmax(func_squared_data > x) / len(func_squared_data)
+#     prob_distrib_function[func] = func_distrib
+#
+#     func_pdf = np.zeros(len(xs) - 2, dtype=float)
+#     for i,x in enumerate(xs[1:-1]):
+#         func_pdf[i] = (func_distrib[i+1] - func_distrib[i-1]) / (xs[i+1] - xs[i-1])
+#     prob_density_function[func] = func_pdf
+#
+#     plt.figure(f'distribution N={N}')
+#     plt.plot(xs, func_distrib, '.', label=func_labels[func])
+#
+#     plt.figure(f'pdf N={N}')
+#     arg_nonzero = np.where(func_pdf > 0)
+#     plt.plot(xs[1:-1][arg_nonzero], func_pdf[arg_nonzero], label=func_labels[func])
+#
+# plt.figure(f'distribution N={N}')
+# plt.title('Cumulative distribution functions')
+# plt.yscale('log')
+# plt.xscale('log')
+# plt.xlabel(r'$x$')
+# plt.ylabel(r'$F(x)$')
+# plt.legend()
+#
+# plt.figure(f'pdf N={N}')
+# plt.title('Probability density function')
+# plt.yscale('log')
+# plt.xscale('linear')
+# plt.xlim(-0.1, 2)
+# plt.legend()
--- plotting/plot_acc_over_x_across_N.py
+++ plotting/plot_acc_over_x_across_N.py
@@ -6,85 +6,217 @@ import os
 from src.utils import DFUtils
 from src.photon_number_distributions import big_F
 
-
-def prefactor(N, func):
-    if func == 'det' or func == 'perm':
-        return 1 / np.sqrt(factorial(N))
-    elif func == 'haf':
-        if N % 2 ==0 :
-            return np.sqrt(comb(N**2 + N - 1, N) / comb(N-1, N/2) / np.power(float(N), 2.*N))
+# <<<<<<<<<<<<<<<<<<< Functions  >>>>>>>>>>>>>>>>>>
+def prefactor(_N, _func):
+    if _func == 'det' or _func == 'perm':
+        return 1 / np.sqrt(factorial(_N))
+    elif _func == 'haf':
+        if _N % 2 == 0:
+            return 1 / np.sqrt(comb(_N - 1, _N / 2) * factorial(_N))
         else:
             return 0
 
-    elif func[:4] == 'lhaf':
-        w = float(func[func.index('w')+2:])
-        return np.sqrt(comb(N**2 + N-1, N) * np.power(2./N**2, float(N)) / big_F(w, N, N)[N])
+    elif _func[:4] == 'lhaf':
+        if _func[_func.index('w') + 2:] == "N^-1":  # currently only recognizes 1/N
+            w = 1 / _N
+        else:
+            w = float(_func[_func.index('w') + 2:])
+        return np.sqrt(np.power(2., float(_N)) / factorial(_N) / big_F(w, _N, _N)[_N])
 
     else:
-        raise ValueError('func not recognized')
+        raise ValueError(f'{_func} not recognized')
 
 
+def get_cdf(data_arr, xs):
+    """cumulative distribution function"""
+    data_arr = np.atleast_2d(data_arr)
+    num_N, num_repeats = data_arr.shape
 
-repeat_i = 1000  #number of data in each raw data file
-repeats=100000
+    cdf = np.zeros((num_N, len(xs)))
+
+    for i, x in enumerate(xs):
+
+        cdf[:, i] = np.argmax(data_arr>x, axis=1) / num_repeats
+
+    return cdf
+
+def read_acc_files(funcs, dir_head, repeats, cdf_density=16):
+    all_func_dirs = os.listdir(dir_head)
+
+    processed_data = {}
+    for func in funcs:
+
+        _func_dirs = [file_ for file_ in all_func_dirs if file_.startswith(func)]
+
+        N_list = []
+        raw_data_list = []
+        for _func_dir in _func_dirs:
+            for N_str in os.listdir(dir_head + fr'\{_func_dir}'):
+                N = int(N_str[2:])
+                if N in N_list:
+                    continue
+
+                #TODO: fix this. this is a temporary solution for a half-run permanent calculation
+                if func=='perm' and N==26:
+                    continue
+
+                N_dir = dir_head + fr'\{_func_dir}\{N_str}'
+                raw_data_files = os.listdir(N_dir)
+
+                combined_raw_data = np.zeros((len(raw_data_files), repeats // len(raw_data_files)), dtype=float)
+                for i, fn in enumerate(raw_data_files):
+                    combined_raw_data[i, :] = np.load(N_dir + fr'\{fn}')
+                combined_raw_data = combined_raw_data.flatten()
+                combined_raw_data.sort()
+
+                N_list.append(N)
+                raw_data_list.append(combined_raw_data)
+
+        N_arr = np.asarray(N_list)
+        raw_data_arr = np.asarray(raw_data_list)
+
+        raw_data_arr = raw_data_arr[np.argsort(N_arr)]
+        N_arr.sort()
+
+        refactored_data_arr = np.zeros((len(N_arr), repeats))
+        for j, N in enumerate(N_arr):
+            refactored_raw_data = prefactor(N, func) * raw_data_arr[j]
+            refactored_data_arr[j,:] = refactored_raw_data
+
+        x_min = np.min(refactored_data_arr)
+        x_min_log = np.floor(np.log10(x_min))
+        x_max_log = 0
+        xs = np.logspace(start=x_min_log, stop=10**x_max_log, num=cdf_density * int(x_max_log - x_min_log) )  # evenly distributed in log scale, 16 per decade
+
+        cum_distrib_arr = get_cdf(refactored_data_arr, xs)
+
+        processed_data[func]={
+            'Ns': N_arr,
+            'xs': xs,
+            'raw_data': raw_data_arr,
+            'refactored_data': refactored_data_arr,
+            'cdf': cum_distrib_arr
+        }
+
+    return processed_data
+
+# <<<<<<<<<<<<<<<<<<< Basic parameters  >>>>>>>>>>>>>>>>>>
+repeats = 100000
 
 dir_head = fr'..\Results\anticoncentration_over_X\{repeats}repeats'
-func_dirs = os.listdir(dir_head)
 
-func = 'perm'
-Ns = np.arange(start=6, stop=26, step=2)
+funcs = ['det', 'haf', 'lhaf_w=1', 'lhaf_w=N^-1', 'perm']
+# funcs = ['haf']
+func_labels = {
+    'det': r'Det',
+    'perm': r'Per',
+    'haf': r'Haf',
+    'lhaf_w=1': r'lHaf$(w=1)$',
+    'lhaf_w=N^-1': r'lHaf$(w=\frac{1}{N})$'
+}
 
-filtered_dir = [file_ for file_ in func_dirs if file_.startswith(func)][0]  # For now just take the first directory, but later need to modify this when data become more complicated
+# xs_toplot = [1., 0.75, 0.56, 0.42, 0.32, 0.24, 0.18, 0.13]  # for cumulative distribution function
+xs_toplot = [1., 0.42, 0.13]
 
-raw_data_arr = np.zeros((len(Ns), repeats), dtype=float)
-refactored_data_arr = np.zeros((len(Ns), repeats), dtype=float)
-plt.figure(f'{func}')
-for j, N in enumerate(Ns):
-    N_dir = dir_head + fr'\{filtered_dir}\N={N}'
-
-    raw_data_files = os.listdir(N_dir)
-
-    combined_raw_data = np.zeros((len(raw_data_files), repeat_i), dtype=float)
-    for i, fn in enumerate(raw_data_files):
-        combined_raw_data[i, :] = np.load(N_dir + fr'\{fn}')
-    combined_raw_data = combined_raw_data.flatten()
-    combined_raw_data.sort()
+save_fig = True
+plt_dir = fr'..\Plots\acc_numerics'
 
 
-    raw_data_arr[j, :] = combined_raw_data
+# <<<<<<<<<<<<<<<<<<< Read data  >>>>>>>>>>>>>>>>>>
+processed_data = read_acc_files(funcs, dir_head, repeats, cdf_density=16)
 
-    pf = prefactor(N, func)
-    refactored_data = pf * combined_raw_data
-    refactored_data_arr[j, :] = refactored_data
+# <<<<<<<<<<<<<<<<<<< Plotting  >>>>>>>>>>>>>>>>>>
+for func in funcs:
+    refactored_data = processed_data[func]['refactored_data']
+    Ns = processed_data[func]['Ns']
 
-    plt.plot(list(range(repeats)), refactored_data, label=f'N={N}')
+    # <<<<<<<<<<<<<<<<<<< Plot distribution of refactored values for each function  >>>>>>>>>>>>>>>>>>
+    plt.figure(f'{func}')
+    for j, N in enumerate(Ns):
 
+        if j % 4 == 0:  # Only plot every 4 of them
+            plt.plot(list(range(repeats)), refactored_data[j,:], label=fr'$N={N}$')
+    x_min, x_max = plt.xlim()
+    plt.axhline(xs_toplot[0], xmin=x_min, xmax=x_max, color='black', linestyle='-')
+    plt.axhline(xs_toplot[-1], xmin=x_min, xmax=x_max, color='black', linestyle='--')
+    plt.text(x_min, xs_toplot[0] * 1.1, fr'$\epsilon={xs_toplot[0]}$')
+    plt.text(x_min, xs_toplot[-1] * 1.1, fr'$\epsilon={xs_toplot[-1]:.3}$')
+    plt.ylabel(r'$\epsilon$')
+    plt.xlabel('trials')
+    plt.legend()
+    plt.yscale('log')
+    plt.xticks([1, repeats])
+    plt.title(rf'distribution of values for |{func_labels[func]}|')
+    if save_fig:
+        plt.savefig(DFUtils.create_filename(plt_dir + fr'\{func}.png'))
+
+    # <<<<<<<<<<<<<<<<<<< For different functions, plot F(N,epsilon) against N for different epsilon >>>>>>>>>>>>>>>>>>
+    plt.figure(fr'{func} $F(N,\epsilon)$')
+
+    cum_distrib = get_cdf(refactored_data, xs_toplot)
+
+    for i, x in enumerate(xs_toplot):
+        plt.plot(Ns, cum_distrib[:, i], label=fr'$\epsilon={x:.3}$')
+
+    plt.legend()
+    plt.xlabel(r'$N$')
+    plt.ylabel(r'$F(N, \epsilon)$')
+    plt.xticks(Ns)
+    plt.xscale('linear')
+    plt.ylim(0, 1)
+    plt.title(rf'$F(N,\epsilon)$ for {func_labels[func]}')
+    if save_fig:
+        plt.savefig(plt_dir + fr'\{func} F(N, x) against N.png')
+
+    # <<<<<<<<<<<<<<<<<<< Plot F(N, epsilon) against N for different functions  >>>>>>>>>>>>>>>>>>
+    x_id = -1
+    x_special = xs_toplot[x_id]
+    plt.figure(fr'$F(N, \epsilon={x_special:.3})$ for different functions')
+
+    plt.plot(Ns, 1 - cum_distrib[:, x_id], label=func_labels[func])
+
+plt.figure(fr'$F(N, \epsilon={x_special:.3})$ for different functions')
+plt.xlabel(r'$N$')
+plt.ylabel(r'$1-F(N,\epsilon)$')
+plt.xscale('linear')
+plt.yscale('log')
+plt.legend()
+plt.title(fr'$1 - F(N, \epsilon={x_special})$ for different functions')
+if save_fig:
+    plt.savefig(plt_dir + fr'\F(N, {x_special:.3}) against N.png')
+
+# <<<<<<<<<<<<<<<<<<< Plot F(N, x) against x for different functions  >>>>>>>>>>>>>>>>>>
+N_special = 22
+plt.figure(fr'$F(N={N_special}, x)$ for different functions')
+for func in funcs:
+    N_id = np.argmax(processed_data[func]['Ns']==N_special)
+    plt.plot(processed_data[func]['xs'], processed_data[func]['cdf'][N_id, :], '.', label=func_labels[func])
+plt.xlabel(r'$x$')
+plt.title(fr'$F(N={N_special}, x)$ for different functions')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+if save_fig:
+    plt.savefig(plt_dir + fr'\F({N_special}, x) against x.png')
+
+
+# <<<<<<<<<<<<<<<<<<< Plot 10^5 function values for different functions  >>>>>>>>>>>>>>>>>>
+plt.figure(f'function values for N={N_special}')
+for func in funcs:
+    refactored_data = processed_data[func]['refactored_data']
+    N_id = np.argmax(processed_data[func]['Ns'] == N_special)
+    plt.plot(list(range(repeats)), refactored_data[N_id,:], label=func_labels[func])
+
+x_min, x_max = plt.xlim()
+plt.axhline(xs_toplot[0], xmin=x_min, xmax=x_max, color='black', linestyle='-')
+plt.axhline(xs_toplot[-1], xmin=x_min, xmax=x_max, color='black', linestyle='--')
+plt.text(x_min, xs_toplot[0] * 1.1, fr'$\epsilon={xs_toplot[0]}$')
+plt.text(x_min, xs_toplot[-1] * 1.1, fr'$\epsilon={xs_toplot[-1]:.3}$')
+plt.ylabel(r'$\epsilon$')
+plt.xlabel('trials')
 plt.legend()
 plt.yscale('log')
 plt.xticks([1, repeats])
-plt.title(func)
-
-
-# Find cumulative distribution function
-
-x_min = np.min(refactored_data_arr)
-x_min_log = np.floor(np.log10(x_min))  # the minimum value much more likely to come from lhaf with w=1
-xs = np.logspace(start=-2, stop=0, num=8)
-
-# first row is xs values, second to last row is cumulative distribution for each N
-cum_distrib = np.zeros((len(Ns) + 1, len(xs)), dtype=float)
-cum_distrib[0, :] = xs
-for i in range(refactored_data_arr.shape[0]):
-
-    for j, x in enumerate(xs):
-        cum_distrib[i+1, j] = np.argmax(refactored_data_arr[i] > x) / repeats
-
-plt.figure(f'{func} F(x,N)')
-for i, x in enumerate(xs):
-    plt.plot(Ns, cum_distrib[1:, i],  label=f'x={x:.3}')
-
-plt.legend()
-plt.xlabel('N')
-plt.yscale('log')
-plt.xscale('log')
-# plt.xticks(Ns)
+plt.title(rf'Distribution of values at N={N_special}')
+if save_fig:
+    plt.savefig(DFUtils.create_filename(plt_dir + fr'\different functions at N={N_special}.png'))
