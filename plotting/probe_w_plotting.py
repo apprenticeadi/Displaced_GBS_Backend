2f7e48c26da56baf2915e9edbf02fcf72d9df334
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-03-31 17:17:35 +0100

    the slope is a half!

--- plotting/probe_w_plotting.py
+++ plotting/probe_w_plotting.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
 from src.utils import DFUtils
 
 date_time = r'\03-03-2023(20-27-26.624767)'
 
 dir = fr'..\Results\probe_w'+date_time
-save_fig = True
+save_fig = False
 plot_dir = dir + r'\plots'
+cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 # This is specific to this simulation. Change it for different simulations. Here I do this to calculate the median.
 n_start = 1
@@ -52,15 +54,20 @@ plt.legend()
 if save_fig:
     plt.savefig(DFUtils.create_filename(plot_dir + r'\histograms.png'))
 
-
+# log_median = np.log(medians[:41, 1])
+# log_mean = np.log(stats[:41, 2])
+# log_min = np.log(stats[:41, 3])
+# log_ns = np.log(stats[:41, 0])
 
 plt.figure('mean value of |tildeX|')
 plt.plot(stats[:, 0], stats[:, 2], 'x', label=r'mean$(|\tilde{X}_{ij}|)$')
+slope, intercept, r_value, p_value, std_err = stats.linregress(log_ns, log_mean)
 plt.plot(medians[:, 0], medians[:, 1], 'x', label=r'median($|\tilde{X}_{ij}|$)')
 plt.plot(stats[:, 0], stats[:, 1], 'x', label=r'min($|\tilde{X}_{ij}|$)')
 plt.plot(stats[:, 0], stats[:, 3], 'x', label=r'std($|\tilde{X}_{ij}|$)')
-plt.plot(stats[:, 0], 1 / stats[:, 0], linestyle='--', color='black', label='1/N')
-plt.plot(stats[:, 0], 1 / stats[:, 0] ** 2, linestyle='-', color='black', label='1/N^2')
+plt.plot(stats[:, 0], 1 / np.sqrt(stats[:, 0]), linestyle='-.', color='black', label=r'$1/\sqrt{N}$')
+plt.plot(stats[:, 0], 1 / stats[:, 0], linestyle='--', color='black', label=r'$1/N$')
+# plt.plot(stats[:, 0], 1 / stats[:, 0] ** 2, linestyle='-', color='black', label=r'$1/N^2$')
 plt.xlabel(r'$N$')
 plt.title(r'Mean and min of $|\tilde{X}_{ij}|$ against $N$')
 # plt.xticks(n_list[::11])
@@ -71,13 +78,13 @@ if save_fig:
     plt.savefig(DFUtils.create_filename(plot_dir + r'\mean_and_min.png'))
 
 # plot std of |Z|
-plt.figure('std of |tildeX|')
-plt.plot(stats[:, 0], stats[:, 3], 'x', label=r'std($|\tilde{X}_{ij}|$)')
-plt.xlabel('N')
-plt.title(r'Std of $|\tilde{X}_{ij}|$ against N')
-plt.xscale('log')
-plt.yscale('log')
-plt.plot(stats[:, 0], 1 / stats[:, 0], linestyle='--', color='black', label='1/sqrt(M)')
-plt.legend()
-if save_fig:
-    plt.savefig(DFUtils.create_filename(plot_dir + r'\std.png'))
+# plt.figure('std of |tildeX|')
+# plt.plot(stats[:, 0], stats[:, 3], 'x', label=r'std($|\tilde{X}_{ij}|$)')
+# plt.xlabel('N')
+# plt.title(r'Std of $|\tilde{X}_{ij}|$ against N')
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.plot(stats[:, 0], 1 / stats[:, 0], linestyle='--', color='black', label='1/sqrt(M)')
+# plt.legend()
+# if save_fig:
+#     plt.savefig(DFUtils.create_filename(plot_dir + r'\std.png'))
