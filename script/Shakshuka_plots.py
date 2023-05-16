12f82d808e98214cd98992802e917f5586a450ab
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-05-16 10:44:08 +0100

    shakshuka plots to probe transition in w

--- script/Shakshuka_plots.py
+++ script/Shakshuka_plots.py
@@ -2,13 +2,16 @@ import numpy as np
 from scipy.stats import unitary_group
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
+import math
 
 from src.utils import MatrixUtils
 # <<<<<<<<<<<<<<<<<<< Size  >>>>>>>>>>>>>>>>>>
-M = 100
+M = 500
 N = int(np.floor(np.sqrt(M)))
 K = N
-w = 1   # The diagonal weight.
+w = 1  # The diagonal weight.
+print(f'M={M}, N={N}, K={K}')
+print(f'w={w}')
 
 U = unitary_group.rvs(M)
 id_K = np.zeros(M)
@@ -21,9 +24,9 @@ x = B / np.outer(half_gamma, half_gamma)
 x = MatrixUtils.filldiag(x, np.zeros(M))  # We don't want x_ii
 masked_x = x[
             ~np.eye(len(x), dtype=bool)]  # mask out diagonal terms which are zero. masked shaped is 1d
-x_n = x[:N, :N]
+x_n = x[:, :N]
 abs_x = np.absolute(x_n)
-sum_x = np.sum(abs_x, axis=1)
+sum_x = np.sum(abs_x, axis=1)  # this doesn't contain all possible sum tilde x
 
 axis_lim = 1e3
 
@@ -31,16 +34,16 @@ plt.figure(r'Plot of $|\tilde\{X\}_ij|$' + f'for M={M}, w={w}')
 plt.axhline(y=0, xmin=-axis_lim, xmax=axis_lim, color='black')
 plt.axvline(x=0, ymin=-axis_lim, ymax=axis_lim, color='black')
 circle1 = plt.Circle((0,0), 1, color='r', alpha=0.5, label='Radius 1')
-circle2 = plt.Circle((0,0), 1/N, color='yellow', alpha=1, label='Radius 1/N')
-circle3 = plt.Circle((0,0), 1/N**4, color='orange', alpha=1, label='Radius 1/N**4')
+circle2 = plt.Circle((0,0), 1/(math.e * (2*N-3)), color='yellow', alpha=1, label='Radius $1/e(2N-3)$')
+# circle3 = plt.Circle((0,0), 1/N**4, color='orange', alpha=1, label='Radius 1/N**4')
 plt.gca().add_patch(circle1)
 plt.gca().add_patch(circle2)
-plt.gca().add_patch(circle3)
+# plt.gca().add_patch(circle3)
 plt.scatter(masked_x.real, masked_x.imag, marker='.')
 plt.xlim(left=-axis_lim, right=axis_lim)
 plt.ylim(bottom=-axis_lim, top=axis_lim)
-plt.yscale('symlog', linthresh = 10**np.floor(np.log10(1/N**4)))
-plt.xscale('symlog', linthresh = 10**np.floor(np.log10(1/N**4)))
+plt.yscale('symlog', linthresh = 10**np.floor(np.log10(1/N**2)))
+plt.xscale('symlog', linthresh = 10**np.floor(np.log10(1/N**2)))
 plt.legend()
 plt.title(r'Scatter plot of $\tilde{X}_{ij}$' + f'for M={M}, w={w}')
 
@@ -74,9 +77,16 @@ beta = np.sqrt(1 - np.sinh(r)**2)
 print(f'r={r}')
 print(f'beta={beta}')
 
+print(f'sinh^2(r)={np.sinh(r)**2}, beta^2={beta**2}')
+
 N_sq = np.sinh(r) ** 2
 N_dis = beta ** 2
 
 sq_dis_ratio = N_sq / N_dis
 
-print(sq_dis_ratio)
+print(f'N_sq / N_dis = {sq_dis_ratio}')
+
+outside_num = np.sum(np.absolute(masked_x) > 1/(math.e * (2*N-3)))
+outside_percentage = outside_num / masked_x.shape[0]
+
+print(f'Outside number = {outside_num} out of {masked_x.shape[0]} with percentage={outside_percentage}')
