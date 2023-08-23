55ffdcb6ad7b2f5f83ff1c8649b8535da4527093
Zhenghao Li <zhenghao.li.1999@gmail.com>
2023-08-23 13:21:26 +0100

    no need to put weight data into graph

--- tests_pass/test_root_finding.py
+++ tests_pass/test_root_finding.py
@@ -18,27 +18,15 @@ adjacency_matrix = MatrixUtils.filldiag(tildeX, np.ones(4))
 lhaf_val = hafnian(adjacency_matrix, loop=True)
 print(lhaf_val)
 
-
 # Construct complete graph
-G = nx.complete_graph(N)
-edge_lists = []
-for i in range(N):
-    # we don't want self loops in the graph, because we are enumerating matchings, not single-pair matchings.
-    for j in range(i+1, N):
-        edge_lists.append((i, j, adjacency_matrix[i,j]))
-G.add_weighted_edges_from(edge_lists)
+G = nx.complete_graph(N)  # this shouldn't have self loops
+assert len(list(nx.selfloop_edges(G))) == 0
 
 # Construct Line graph
 L = nx.line_graph(G)  # the graph, node and edge data are not propagated. But the nodes in L are now tuples of the form (u,v) for u,v in G
-L.add_nodes_from((node, G.edges[node]) for node in L)
-# check with
-# L.nodes(data=True)
 
 # Construct complement of line graph
 LC = nx.complement(L)
-LC.add_nodes_from((node, L.nodes[node]) for node in LC)
-# check with
-# LC.nodes(data=True)
 
 coeffs = np.zeros(int(N/2) + 1, dtype=complex)  # highest degree is k=floor(N/2), and there is also zero-degree.
 coeffs[0] = 1  # the polynomial zero-th degree coeff is 1, since product of diagonal terms is 1
@@ -50,7 +38,7 @@ for clique in nx.enumerate_all_cliques(LC):
     k = len(clique)
     c = 1.
     for node in clique:
-        c = c * LC.nodes[node]['weight']
+        c = c * adjacency_matrix[node]
 
     coeffs[k] += c
 
