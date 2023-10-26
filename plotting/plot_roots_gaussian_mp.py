ea160e5664afe64b4df393c109d503c9e33fb341
Li <zl4821@ic.ac.uk>
2023-10-26 13:20:53 +0100

    add bootstrapping

--- plotting/plot_roots_gaussian_mp.py
+++ plotting/plot_roots_gaussian_mp.py
@@ -1,113 +1,82 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from src.utils import DFUtils
+from scipy.stats import bootstrap
+from src.utils import DFUtils, DGBSUtils
 import datetime
+import time
 
 total_Ns = list(range(4, 17))
+reps = 10000
 
 Ns_dir_dict = {
-    r'..\Results\roots_matching_polynomial\10000rep_2023-08-23(18-00-43.689530)': list(range(4, 15)),
-    r'..\Results\roots_matching_polynomial\10000rep_2023-08-23(19-35-17.526866)': [15],
-    r'..\Results\roots_matching_polynomial\10000rep_2023-08-25(17-08-36.941174)': [16]
+    rf'..\Results\roots_matching_polynomial\{reps}rep_2023-08-23(18-00-43.689530)': list(range(4, 15)),
+    rf'..\Results\roots_matching_polynomial\{reps}rep_2023-08-23(19-35-17.526866)': [15],
+    rf'..\Results\roots_matching_polynomial\{reps}rep_2023-08-25(17-08-36.941174)': [16]
 }
 
 save_fig = True
 time_stamp = datetime.datetime.now().strftime("%Y-%m-%d(%H-%M-%S.%f)")
 plot_dir = fr'..\Plots\roots_mp\tildeX_distrib_{time_stamp}'
 
-roots_dict = {}
-min_roots_mp_dict={}
-min_roots = np.zeros(len(total_Ns), dtype=float)
-max_roots = np.zeros(len(total_Ns), dtype=float)
-median_roots = np.zeros(len(total_Ns), dtype=float)
-median_min_roots_mp = np.zeros(len(total_Ns), dtype=float)
+def find_quart(a, axis=0):
+    return np.quantile(a, 0.25, axis=axis)
+
+# Find the min root of each of the 10000 matching polynomials. Bootstrap find their median and first quartile.
+min_roots = np.zeros((len(total_Ns), reps), dtype=np.complex128)  # the smallest root of each distinct matching polynomial
+abs_min_roots = np.zeros((len(total_Ns), reps), dtype=float)
+
+median_min_roots = np.zeros((len(total_Ns), 3), dtype=float)  # median of min_roots_mp and their confidence interval
+quart_min_roots = np.zeros((len(total_Ns), 3), dtype=float)  # first quartile of min_roots_mp and their confidence interval
 for results_dir in Ns_dir_dict.keys():
     Ns = Ns_dir_dict[results_dir]
     for N in Ns:
         roots_N = np.load(results_dir + fr'\N={N}_roots.npy')
-        abs_roots_N = np.abs(roots_N)
-
-        roots_dict[N] = roots_N  # this is all the roots
-
         i_N = total_Ns.index(N)
 
-        min_roots[i_N] = np.min(abs_roots_N)
-        max_roots[i_N] = np.max(abs_roots_N)
-        median_roots[i_N] = np.median(abs_roots_N)
-
         # the min root from each distinct matching polynomial
-        roots_N_mp = roots_N.reshape((10000, N//2))
-        row_args = np.abs(roots_N_mp).argmin(axis=1)  # this probably is always the last element
-        min_roots_mp = np.zeros(10000, dtype=np.complex128)
+        roots_N = roots_N.reshape((reps, N // 2))
+        row_args = np.abs(roots_N).argmin(axis=1)
+        min_roots_N = np.zeros(reps, dtype=np.complex128)
         for i_row, row_arg in enumerate(row_args):
-            min_roots_mp[i_row] = roots_N_mp[i_row, row_arg]
-        min_roots_mp_dict[N] = min_roots_mp
-        median_min_roots_mp[i_N] = np.median(np.abs(min_roots_mp))
+            min_roots_N[i_row] = roots_N[i_row, row_arg]
+        min_roots[i_N] = min_roots_N
+
+        abs_roots_N = np.abs(min_roots_N)
+        abs_min_roots[i_N] = abs_roots_N
+
+        print(f'Bootstrapping median for N={N}')
+        # bootstrapping
+        t1 = time.time()
+        median = np.median(abs_roots_N)
+        med_res = bootstrap((abs_roots_N,), np.median, confidence_level=0.95)
+        median_min_roots[i_N, :] = (median, median - med_res.confidence_interval.low, med_res.confidence_interval.high - median)
+        t2 = time.time()
+        print(f'Bootstrapping finished after {t2-t1}s. Bootstrapping first quartile.')
+
+        quart = find_quart(abs_roots_N)
+        quart_res = bootstrap((abs_roots_N,), find_quart, confidence_level=0.95)
+        quart_min_roots[i_N, :] = (quart, quart - quart_res.confidence_interval.low, quart_res.confidence_interval.high - quart)
+        t3 = time.time()
+        print(f'Bootstrapping finished after {t3-t2}s. ')
+
+np.save(DFUtils.create_filename(plot_dir + f'\median_min_roots_mp.npy'), median_min_roots)
+np.save(plot_dir + f'\quart_min_roots_mp.npy', quart_min_roots)
 
 # <<<<<<<<<<<<<<<<<<< Plotting  >>>>>>>>>>>>>>>>>>
-log_linthresh = int(np.log10(np.min(min_roots))) - 1
+log_linthresh = -4
 linthresh = 10 ** (log_linthresh)
-axis_log_lim = int(np.log10(np.max(max_roots))) + 1
+axis_log_lim = 1
 axis_lim = 10 ** (axis_log_lim)
-half_axis_ticks = 10 ** np.arange(log_linthresh, axis_log_lim+1, step=2, dtype=np.float64)
+half_axis_ticks = 10 ** np.arange(log_linthresh, axis_log_lim + 1, step=2, dtype=np.float64)
 axis_ticks = np.concatenate([-half_axis_ticks, half_axis_ticks, [0]])
-
-
-for i_N, N in enumerate(total_Ns):
-    save_name = plot_dir + fr'\N={N}.png'
-
-    # Create a scatter plot in the complex plane
-    roots_real = [np.real(r) for r in roots_dict[N]]
-    roots_imag = [np.imag(r) for r in roots_dict[N]]
-
-    plt.figure(f'N={N}')
-
-    # Remove plot boundaries (spines)
-    ax = plt.gca()  # Get the current Axes
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-
-    # Create solid lines for the real and imaginary axes
-    plt.axhline(0, color='black', linewidth=1)  # Real axis
-    plt.axvline(0, color='black', linewidth=1)  # Imaginary axis
-
-    # Scatter plot roots
-    plt.scatter(roots_real, roots_imag, marker='x', s=10)
-
-    # create circular boundary of median roots
-    median_root = median_roots[i_N]
-    circle_median = plt.Circle((0, 0), median_roots[i_N], color='r', fill=False, label=fr'$|z|={{{median_root:.3f}}}$')
-    plt.gca().add_patch(circle_median)
-
-    plt.xscale('symlog', linthresh=linthresh)
-    plt.yscale('symlog', linthresh=linthresh)
-
-    # Set axes
-    plt.xlim(-axis_lim, axis_lim)
-    plt.ylim(-axis_lim, axis_lim)
-    plt.xticks(axis_ticks)
-    plt.yticks(axis_ticks)
-
-    plt.xlabel(r'Re$(z)$')
-    plt.ylabel(r'Im$(z)$')
-
-    plt.legend(loc='upper right')
-
-    plt.show()
-
-    if save_fig:
-        plt.savefig(DFUtils.create_filename(save_name))
-
 
 # plot the min root of each matching polynomial
 for i_N, N in enumerate(total_Ns):
-    save_name = plot_dir + fr'\min_root_of_each_mp_N={N}.png'
+    save_name = plot_dir + fr'\min_root_of_each_mp_N={N}.pdf'
 
     # Create a scatter plot in the complex plane
-    roots_real = [np.real(r) for r in min_roots_mp_dict[N]]
-    roots_imag = [np.imag(r) for r in min_roots_mp_dict[N]]
+    roots_real = [np.real(r) for r in min_roots[i_N]]
+    roots_imag = [np.imag(r) for r in min_roots[i_N]]
 
     plt.figure(f'min root of each mp N={N}')
 
@@ -126,9 +95,13 @@ for i_N, N in enumerate(total_Ns):
     plt.scatter(roots_real, roots_imag, marker='x', s=10)
 
     # create circular boundary of median roots
-    median_root = median_min_roots_mp[i_N]
-    circle_median = plt.Circle((0, 0), median_root, color='r', fill=False, label=fr'$|z|={{{median_root:.3f}}}$')
+    median_root = median_min_roots[i_N, 0]
+    circle_median = plt.Circle((0, 0), median_root, color='black', fill=False, label=fr'$|z|={{{median_root:.3f}}}$')
     plt.gca().add_patch(circle_median)
+
+    quart_root = quart_min_roots[i_N, 0]
+    circle_quart = plt.Circle((0, 0), quart_root, color='red', fill=False, label=fr'$|z|={{{quart_root:.3f}}}$')
+    plt.gca().add_patch(circle_quart)
 
     plt.xscale('symlog', linthresh=linthresh)
     plt.yscale('symlog', linthresh=linthresh)
@@ -139,8 +112,8 @@ for i_N, N in enumerate(total_Ns):
     plt.xticks(axis_ticks)
     plt.yticks(axis_ticks)
 
-    plt.xlabel(r'Re$(z)$')
-    plt.ylabel(r'Im$(z)$')
+    plt.xlabel(r'Re')
+    plt.ylabel(r'Im')
 
     plt.legend(loc='upper right')
 
@@ -150,51 +123,88 @@ for i_N, N in enumerate(total_Ns):
         plt.savefig(DFUtils.create_filename(save_name))
 
 
-plt.figure('min and max |root|')
-plt.plot(total_Ns, min_roots, '.', label='min')
-# plt.plot(total_Ns, max_roots, '.', label='max')
-#TODO: bootstrap estimate this
-plt.plot(total_Ns, median_roots, '.', label='median')
-plt.plot(total_Ns, median_min_roots_mp, '.', label='median(min)')
+plt.figure('medians')
+
+plt.errorbar(total_Ns, median_min_roots[:, 0], yerr=median_min_roots[:, 1:].T, fmt='x', color='black', linestyle='None', label='median')
+plt.errorbar(total_Ns, quart_min_roots[:, 0], yerr=quart_min_roots[:, 1:].T, fmt='x', color='red', linestyle='None', label='first quartile')
 plt.xlabel(r'$N$')
-plt.yscale('log')
+plt.ylabel(r'|z|')
 plt.legend()
 if save_fig:
-    plt.savefig(DFUtils.create_filename(plot_dir + r'\min_and_max_root.png'))
+    plt.savefig(DFUtils.create_filename(plot_dir + r'\medians.pdf'))
 
 # Colour plot
 w_max = 5.000
 w_min = 0.0001
 ws = np.linspace(w_min, w_max, 10000)
-epsilons = 1 / ws**2
+epsilons = 1 / ws ** 2
 epsilons = epsilons[::-1]
-
-abs_min_roots_mp_arr = np.zeros((len(total_Ns), 10000), dtype=float)  # each row is the 10,000 abs(min(roots)) for each N
-for i_N, N in enumerate(total_Ns):
-    abs_min_roots_mp_arr[i_N] = np.abs(min_roots_mp_dict[N])
 
 color_mesh = np.zeros((len(total_Ns), len(epsilons)), dtype=float)
 for i_eps, epsilon in enumerate(epsilons):
-    counts = np.sum(abs_min_roots_mp_arr <= epsilon, axis=1)
-    color_mesh[:, i_eps] = counts / 10000
+    counts = np.sum(abs_min_roots <= epsilon, axis=1)
+    color_mesh[:, i_eps] = counts / reps
 
 plt.figure('color plot Pr(hit a zero)')
-plt.pcolormesh(np.concatenate([[2*ws[-1]-ws[-2]], ws[::-1]]), np.arange(min(total_Ns), max(total_Ns)+2)-0.5, color_mesh, vmin=0, vmax=1)
+plt.pcolormesh(np.concatenate([[2 * ws[-1] - ws[-2]], ws[::-1]]), np.arange(min(total_Ns), max(total_Ns) + 2) - 0.5,
+               color_mesh, vmin=0, vmax=1)
 plt.colorbar()
 plt.xlabel(r'$|w|$')
 plt.ylabel(r'$N$')
 plt.xscale('linear')
-plt.ylim([min(total_Ns)-0.5, max(total_Ns)+0.5])
+plt.ylim([min(total_Ns) - 0.5, max(total_Ns) + 0.5])
 plt.yticks(total_Ns[::2])
 plt.xlim([min(ws), max(ws)])
 for N in total_Ns:
-    plt.axhline(N-0.5, xmin=0, xmax=w_max, color='white', linestyle=':')
+    plt.axhline(N - 0.5, xmin=0, xmax=w_max, color='white', linestyle=':')
 
-#TODO: bootstrap these
-plt.plot(1 / np.sqrt(median_min_roots_mp), total_Ns, 'x', color='red')  # this is 50%
+# Errorbar plots of median and first quantile
+med_ws = 1 / np.sqrt(median_min_roots[:, 0])
+med_ws_err = np.zeros((2, len(med_ws)), dtype=float)
+for i_N in range(len(median_min_roots)):
+    med_ws_err[0, i_N] = 0.5 * median_min_roots[i_N, 1] / median_min_roots[i_N, 0] * med_ws[i_N]  # lower error
+    med_ws_err[1, i_N] = 0.5 * median_min_roots[i_N, 2] / median_min_roots[i_N, 0] * med_ws[i_N] # upper error
+plt.errorbar(med_ws, total_Ns, xerr=med_ws_err, fmt='x', color='black', linestyle='None')  # this is 50%
 
-arg_quarter = np.argmax(color_mesh>=0.25, axis=1)
-ws_reverse = ws[::-1]
-plt.plot(ws_reverse[arg_quarter], total_Ns, 'x', color='black')  # this is 25%
+quart_ws = 1 / np.sqrt(quart_min_roots[:, 0])
+quart_ws_err = np.zeros((2, len(quart_ws)), dtype=float)
+for i_N in range(len(quart_min_roots)):
+    quart_ws_err[0, i_N] = 0.5 * quart_min_roots[i_N, 1] / quart_min_roots[i_N, 0] * quart_ws[i_N] # lower error
+    quart_ws_err[1, i_N] = 0.5 * quart_min_roots[i_N, 2] / quart_min_roots[i_N, 0] * quart_ws[i_N] # upper error
+plt.errorbar(quart_ws, total_Ns, xerr=quart_ws_err, fmt='x', color='red', linestyle='None')  # this is 25%
 
-plt.savefig(DFUtils.create_filename(plot_dir + r'\color_mesh_prob_hit_a_zero.png'))
+if save_fig:
+    plt.savefig(DFUtils.create_filename(plot_dir + r'\color_mesh_prob_hit_a_zero.pdf'))
+
+
+def solve_photon_ratio(w, lower_err, upper_err):
+
+    r, beta = DGBSUtils.solve_w(w, N_mean=1)
+    ratio = beta**2 / np.sinh(r)**2
+
+    r1, beta1 = DGBSUtils.solve_w(w-lower_err, N_mean=1)
+    ratio1 = beta1**2 / np.sinh(r1)**2
+
+    r2, beta2 = DGBSUtils.solve_w(w+upper_err, N_mean=1)
+    ratio2 = beta2**2 / np.sinh(r2)**2
+
+    return ratio, ratio-ratio1, ratio2-ratio
+
+# solve for photon ratio
+med_ratios = np.zeros((len(total_Ns), 3), dtype=float) # include error
+quart_ratios = np.zeros((len(total_Ns), 3), dtype=float)
+for i_N, N in enumerate(total_Ns):
+    med_ratios[i_N, :] = solve_photon_ratio(med_ws[i_N], med_ws_err[0, i_N], med_ws_err[1, i_N])
+    quart_ratios[i_N, :] = solve_photon_ratio(quart_ws[i_N], quart_ws_err[0, i_N], quart_ws_err[1, i_N])
+
+plt.figure('Photon ratios')
+plt.errorbar(total_Ns, med_ratios[:, 0], yerr=med_ratios[:, 1:].T, fmt='x', color='black', linestyle='None')
+plt.errorbar(total_Ns, quart_ratios[:, 0], yerr=quart_ratios[:, 1:].T, fmt='x', color='red', linestyle='None')
+plt.ylabel(r'$|\beta|^2/\sinh^2(r)$')
+plt.xlabel(r'$N$')
+
+if save_fig:
+    plt.savefig(DFUtils.create_filename(plot_dir + r'\photon_ratios.pdf'))
+
+
+
