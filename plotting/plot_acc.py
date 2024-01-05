5eb3c3868410b2ffbe32ed4c46e6573d0cc14770
Li <zhenghao.li.1999@gmail.com>
2024-01-05 11:30:04 +0000

    enable bootstrapping

--- plotting/plot_acc.py
+++ plotting/plot_acc.py
@@ -5,6 +5,7 @@ from scipy.stats import bootstrap
 import os
 import datetime
 import time
+import pandas as pd
 
 from src.utils import DFUtils, DGBSUtils
 from src.photon_number_distributions import big_F
@@ -113,7 +114,7 @@ def get_cdf(data, x, bootstrapping = False, confidence_level=0.9):
     if bootstrapping:
         print('Start bootstrapping')
         t1 = time.time()
-        bootstrap_res = bootstrap((data_converted,), np.mean, confidence_level=confidence_level)
+        bootstrap_res = bootstrap((data_converted,), np.mean, confidence_level=confidence_level, batch=100, method='basic')
         t2 = time.time()
         print(f'end bootstrapping after {t2-t1}s')
         n_error = cdf - bootstrap_res.confidence_interval.low
@@ -179,16 +180,17 @@ print('Read data finished')
 # <<<<<<<<<<<<<<<<<<< Plotting  >>>>>>>>>>>>>>>>>>
 plt.rcParams.update({'font.size': 8})
 cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
-
-# <<<<<<<<<<<<<<<<<<< Plot cdf[1/epsilon] against epsilon  >>>>>>>>>>>>>>>>>>
-Ns = np.arange(6, 35)
 fontsize = 16
+Ns = np.arange(6, 35)
 funcs_to_plot = ['det', 'perm', 'haf', 'lhaf_w=0.1', 'lhaf_w=1']
+
+# # <<<<<<<<<<<<<<<<<<< Plot cdf[1/epsilon] against epsilon  >>>>>>>>>>>>>>>>>>
+epsilons = np.logspace(start=0, stop=2, num=20, base=10)
+np.save(DFUtils.create_filename(plt_dir + rf'\data\epsilons.npy'), epsilons)
 for i_N, N in enumerate(Ns):
 
     print(f'Plot cdf(1/epsilon) against epsilon for N={N}')
 
-    epsilons = np.logspace(start=0, stop=6, num=20, base=10)
     for i_func, func in enumerate(funcs_to_plot):
         func_label = func_labels(func)
 
@@ -206,36 +208,35 @@ for i_N, N in enumerate(Ns):
                 t1 = time.time()
                 cdf, n_error, p_error = get_cdf(refactored_data, 1/epsilon, bootstrapping=bootstrapping)
                 t2 = time.time()
-                print(f'Calculate finished after {t2-t1}s. Results={(cdf, n_error, p_error)}')
+                # print(f'Calculate finished after {t2-t1}s. Results={(cdf, n_error, p_error)}')
 
                 cdfs[i_epsilon] = cdf
                 error_bars[:, i_epsilon] = np.array([n_error, p_error])
 
-            # np.save(DFUtils.create_filename(plt_dir + rf'\cdf_data\{func}\N={N}_cdfs.npy'), cdfs)
-            # np.save(DFUtils.create_filename(plt_dir + rf'\cdf_data\{func}\N={N}_errors.npy'), cdfs)
+            np.save(DFUtils.create_filename(plt_dir + rf'\data\{func}\N={N}_cdfs.npy'), cdfs)
+            np.save(DFUtils.create_filename(plt_dir + rf'\data\{func}\N={N}_errors.npy'), error_bars)
 
-            # plt.figure(f'cdf for N={N}')
-            # plt.errorbar(epsilons, cdfs, fmt='x', ls='none', yerr=error_bars, color=cycle[i_func], label=func_label)
-
+            plt.figure(f'cdf for N={N}')
+            plt.errorbar(epsilons, cdfs, fmt='o', ls='none', yerr=error_bars, color=cycle[i_func], label=func_label)
 
             plt.figure(f'cdf for {func}')
-            plt.plot(epsilons, cdfs, 'x', ls='None', label=f'N={N}')
+            plt.errorbar(epsilons, cdfs, fmt='o', ls='None', yerr=error_bars, label=f'N={N}')
 
         else:
             continue
 
-    # plt.figure(f'cdf for N={N}')
-    # plt.xlabel(r'$\epsilon$', fontsize=fontsize)
-    # plt.ylabel(r'$F(N, 1/\epsilon)$', fontsize=fontsize)
-    # plt.tick_params(axis='both', which='major', labelsize=fontsize-4)
-    # plt.yscale('log')
-    # plt.xscale('log')
-    # plt.ylim(1e-6, 2)
-    # if i_N == 0:
-    #     plt.legend(fontsize=fontsize-4)
-    #
-    # if save_fig:
-    #     plt.savefig(DFUtils.create_filename(plt_dir + rf'\cdf_for_N={N}.pdf'))
+    plt.figure(f'cdf for N={N}')
+    plt.xlabel(r'$\epsilon$', fontsize=fontsize)
+    plt.ylabel(r'$F(N, 1/\epsilon)$', fontsize=fontsize)
+    plt.tick_params(axis='both', which='major', labelsize=fontsize-4)
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.ylim(1e-6, 2)
+    if i_N == 0:
+        plt.legend(fontsize=fontsize-4)
+
+    if save_fig:
+        plt.savefig(DFUtils.create_filename(plt_dir + rf'\cdf_for_N={N}.pdf'))
 
 for func in funcs_to_plot:
     plt.figure(f'cdf for {func}')
@@ -250,10 +251,84 @@ for func in funcs_to_plot:
     if func == 'det':
         plt.legend(fontsize=fontsize-6)
     if save_fig:
-        plt.savefig(plt_dir + rf'\cdf_for_{func}.pdf')
+        plt.savefig(DFUtils.create_filename(plt_dir + rf'\cdf_for_{func}.pdf'))
 
 
+# # <<<<<<<<<<<<<<<<<<< Plot 1-cdf against N  >>>>>>>>>>>>>>>>>>
+special_eps_id = [0, 3, 6]
+
+for eps_id in special_eps_id:
+    epsilon = epsilons[eps_id]
+
+    plt.figure(rf'1-cdf for eps={epsilon}')
+
+    for func in funcs_to_plot:
+        inv_cdfs = []
+        for N in Ns:
+            if N in multi_func_raw_data[func].keys():
+                cdfs = np.load(plt_dir + rf'\data\{func}\N={N}_cdfs.npy')
+                error_bars = np.load(plt_dir + rf'\data\{func}\N={N}_errors.npy')
+
+                inv_cdfs.append({
+                    'N': N,
+                    '1-cdf': 1 - cdfs[eps_id],
+                    'n_error': error_bars[1, eps_id],
+                    'p_error': error_bars[0, eps_id]
+                })
+
+            else:
+                continue
+
+        inv_cdfs_df = pd.DataFrame(inv_cdfs)
+        inv_cdfs_df.to_csv(DFUtils.create_filename(plt_dir + rf'\data\{func}\1-cdf(N, eps={epsilon}).csv'))
+
+        plt.errorbar(inv_cdfs_df['N'], np.log(list(inv_cdfs_df['1-cdf'])), yerr=[inv_cdfs_df['n_error'], inv_cdfs_df['p_error']],
+                     capsize=1, label=func_labels(func), ls='None', fmt='x')
+
+    plt.legend(fontsize=fontsize - 4, loc='lower left')
+    plt.ylabel(rf'$\ln(1-F(N, {{{1/epsilon:.2f}}})$', fontsize=fontsize)
+    plt.xlabel('$N$', fontsize=fontsize)
+    plt.tick_params(axis='both', which='major', labelsize=fontsize - 4)
+    plt.savefig(plt_dir + rf'\1-cdf(N, eps={epsilon:.2f}).pdf')
+    plt.ylim(-6, -0.1)
 
 
+# # <<<<<<<<<<<<<<<<<<< Bootstrap mean  >>>>>>>>>>>>>>>>>>
+for i_func, func in enumerate(funcs_to_plot):
 
+    results_rows = []
+    for i_N, N in enumerate(Ns):
 
+        if N in multi_func_raw_data[func].keys():
+            # Refactor data
+            raw_data = multi_func_raw_data[func][N]
+            refactored_data = prefactor(N, func) * raw_data
+
+            refactored_mean = np.mean(refactored_data)
+
+            t1 = time.time()
+            bootstrap_res = bootstrap((refactored_data,), np.mean, confidence_level=0.9, batch=100, method='basic')
+            t2 = time.time()
+            print(f'Bootstrapping {func} for N={N} finished after {t2-t1}s')
+
+            results_rows.append({'N': N, 'mean': refactored_mean,
+                                 'n_error': refactored_mean - bootstrap_res.confidence_interval.low,
+                                 'p_error': bootstrap_res.confidence_interval.high - refactored_mean}
+                                )
+        else:
+            continue
+
+    results_df = pd.DataFrame(results_rows)
+    results_df.to_csv(DFUtils.create_filename(plt_dir + rf'\data\{func}\bootstrapping_df.csv'), index=False)
+
+    plt.figure('Mean')
+    plt.errorbar(results_df['N'], results_df['mean'], yerr=[results_df['n_error'], results_df['p_error']], capsize=5, label=func_labels(func))
+
+fig=plt.figure('Mean')
+plt.legend(fontsize=fontsize-4, loc='upper right')
+plt.ylim([0, 1])
+plt.xlabel('$N$', fontsize=fontsize)
+plt.ylabel('Refactored mean', fontsize=fontsize)
+plt.tick_params(axis='both', which='major', labelsize=fontsize-4)
+fig.set_size_inches(10, 6, forward=True)
+plt.savefig(plt_dir + rf'\means.pdf')
