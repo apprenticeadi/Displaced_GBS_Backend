9a1881eec00c7a0cbe50232f7735a9a3d11fd3b3
Li <zhenghao.li.1999@gmail.com>
2024-02-26 11:22:02 +0000

    add progress bar

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import logging
 import datetime
 import copy
 from scipy.optimize import fsolve
+import time
 
 import src.interferometer as itf
 
@@ -289,3 +290,25 @@ class DGBSUtils:
             w = float(w_label[2:])
 
         return w
+
+
+def progressbar(it, prefix="", size=60, out=sys.stdout):
+    """Function for progress bar. https://stackoverflow.com/questions/3160699/python-progress-bar """
+
+    count = len(it)
+    start = time.time()
+
+    def show(j):
+        x = int(size * j / count)
+        remaining = ((time.time() - start) / j) * (count - j)
+
+        mins, sec = divmod(remaining, 60)
+        time_str = f"{int(mins):02}:{sec:05.2f}"
+
+        print(f"{prefix}[{u'█' * x}{('.' * (size - x))}] {j}/{count} Est wait {time_str}", end='\r', file=out,
+              flush=True)
+
+    for i, item in enumerate(it):
+        yield item
+        show(i + 1)
+    print("\n", flush=True, file=out)
